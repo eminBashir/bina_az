1c379860b1ad5bb07f4bc9bed6a204063fe57cef
Emin <emin@example.com>
2025-03-16 13:38:57 +0400

    done

---
+++ Bina_az/DataLayer/Extensions/DbInject.cs
@@ -0,0 +1,23 @@
+﻿using DataLayer.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Extensions
+{
+    public static class DbInject
+    {
+        public static void AddServices(this IServiceCollection services, IConfigurationManager configuration)
+        {
+            services.AddDbContext<AppDbContext>(op =>
+            {
+                op.UseSqlServer(configuration.GetConnectionString("Default"));
+            });
+        }
+    }
+}
